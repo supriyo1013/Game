8fef339af0159ca055c06f25c46529653e8869db
supriyo1013 <gantaitsupriyo@gmail.com>
2025-08-01 15:33:28 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,50 +1,53 @@
 
-let i=0;
 let step=2;
 let result="";
-let fruit=["Lichi","Coconut","Mango","Banana","WaterMalon","Apple","Jacfruit","Papaa","Orange"];
+let sum = 1;
+let fruits = [
+    {id : 28,name : "Apple",},
+    {id : 40,name : "Banana",},
+    {id : 32,name : "Coconut",},
+    {id : 22,name : "Jacfruit",},
+    {id : 31,name : "Lichi",},
+    {id : 24,name : "Mango",},
+    {id : 38,name : "Orange",},
+    {id : 30,name : "Papaa",},
+    {id : 26,name : "WaterMalon",},
+];
 
 function nstep() {
-    // step++;
     document.querySelectorAll("div").forEach((el)=> el.classList.remove("active"));
     document.getElementById(`div${step}`).classList.add("active");
     document.getElementById("btns").classList.add("active");
 }
 
 function yesStep() {
-    if(step == 9){
-        i++;
-        // result=fruit[i];
-        // document.querySelectorAll("div").forEach((el)=> el.classList.remove("active"));
-        // document.getElementById("result").classList.add("active");
-        // document.getElementById("restart").classList.add("active");
-        // document.querySelector("h1").innerText=`${result}`;
+    sum+=step;
+    step++;
+    if(step == 10){
         showResult();
     }
     else{
-        i++;
-        step++;
         nstep();
     }
 }
 
 function noStep() {
-    if(step == 9){
-        // result=fruit[i];
-        // document.querySelectorAll("div").forEach((el)=> el.classList.remove("active"));
-        // document.getElementById("result").classList.add("active");
-        // document.getElementById("restart").classList.add("active");
-        // document.querySelector("h1").innerText=`${result}`;
+    step++;
+    if(step == 10){
         showResult();
     }
     else{
-        step++;
         nstep();
     }
 }
 
 function showResult() {
-    result=fruit[i];
+    const fruit = fruits.find(f => f.id == sum);
+    if(fruit){
+        result=fruit.name;
+    }else{
+        result = "You give some wrong information";
+    }
     document.querySelectorAll("div").forEach((el)=> el.classList.remove("active"));
     document.getElementById("result").classList.add("active");
     document.getElementById("restart").classList.add("active");
@@ -53,7 +56,7 @@ function showResult() {
 
 function restart() {
     step=1;
-    i=0;
+    sum=1;
     document.querySelectorAll("div").forEach((el)=> el.classList.remove("active"));
     document.getElementById(`div${step}`).classList.add("active");
     step++;
