13c93a102daa1d360c8144c67fd05ab1929cf3f7
supriyo1013 <gantaitsupriyo@gmail.com>
2025-08-01 16:29:48 +0530

    Update app.js

--- app.js
+++ app.js
@@ -42,21 +42,26 @@ function noStep() {
 }
 
 function showResult() {
-    const fruit = fruits.find(f => f.id == sum);
-    if(fruit){
-        result=fruit.name;
-    }else{
-        result = "You give some wrong information";
-    }
+    
     document.querySelectorAll("div").forEach((el)=> el.classList.remove("active"));
     document.getElementById("result").classList.add("active");
     document.getElementById("restart").classList.add("active");
-    document.querySelector("h1").innerText=`${result}`;
+    const fruit = fruits.find(f => f.id == sum);
+    if(fruit){
+        result=fruit.name;
+        document.querySelector("h1").innerText=`${result}`;
+    }else{
+        result = "You give some wrong information";
+        document.querySelector(".wrong").innerText=`${result}`;
+    }
 }
 
 function restart() {
     step=1;
     sum=1;
+    result="";
+    document.querySelector("h1").innerText=`${result}`;
+    document.querySelector(".wrong").innerText=`${result}`;
     document.querySelectorAll("div").forEach((el)=> el.classList.remove("active"));
     document.getElementById(`div${step}`).classList.add("active");
     step++;
